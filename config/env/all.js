5aef1a13a98631a3f83f83b2a04d9b72954ec702
Sergio Puleri <spuleri@ufl.edu>
2015-02-08 21:44:05 -0500

    Add bootflat and style signup/signin

--- config/env/all.js
+++ config/env/all.js
@@ -14,7 +14,9 @@ module.exports = {
 		lib: {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				// 'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				//bootflat css, needed to comment out the bootstrap theme to use the Bootflat one
+				'public/lib/Bootflat/bootflat/css/bootflat.min.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
@@ -25,7 +27,11 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				//bootflat js
+				'public/lib/Bootflat/bootflat/js/icheck.min.js',
+				'public/lib/Bootflat/bootflat/js/jquery.fs.selector.min.js',
+				'public/lib/Bootflat/bootflat/js/jquery.fs.stepper.min.js'
 			]
 		},
 		css: [
