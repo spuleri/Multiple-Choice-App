2b3e6da276abe9ca61fb5c1a6789c81366a64b0c
Dylan Richardson <dylanrichardson47@gmail.com>
2015-02-15 21:54:03 -0500

    Add some of the framework of the document schemas

--- app/models/course.server.model.js
+++ app/models/course.server.model.js
@@ -1,10 +1,65 @@
 'use strict';
 
 /**
+ * ---------------------NOTE----------------------
+ * This is an embedded data model, containing not
+ * just courses, but other vital components of the
+ * application, like so:
+ * Course               |
+ * --Quiz Array         |   All are schemas
+ * ----Question Array   |
+ * ------Answer Array   |
+ */
+
+/**
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
+
+/**
+ * Potential Answer Schema
+ */
+var ansSchema = new Schema({
+    name: {
+        type: String,
+        default: '',
+        required: 'Please name the quiz',
+        trim: true
+    },
+    valid: Boolean
+});
+
+/**
+ * Question Schema
+ */
+//TODO: Make Question Schema
+var questionSchema = new Schema({
+    title: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    description: {
+        type: String,
+        default: '',
+        required: 'Please give the question a description',
+        trim: true
+    },
+    answers: [ansSchema]
+});
+/**
+ * Quiz Schema
+ */
+var quizSchema = new Schema ({
+    name: {
+        type: String,
+        default: '',
+        required: 'Please name the quiz',
+        trim: true
+    },
+    questions: [questionSchema]
+});
 
 /**
  * Course Schema
@@ -23,7 +78,10 @@ var CourseSchema = new Schema({
 	owner: {
 		type: Schema.ObjectId,
 		ref: 'User'
-	}
+	},
+    quizzes: {
+
+    }
 });
 
 mongoose.model('Course', CourseSchema);
