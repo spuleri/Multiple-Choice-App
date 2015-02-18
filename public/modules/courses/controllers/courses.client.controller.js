bedd3501b3712d4d6b64acd41428e3ccec28ffab
Fai <cfkwok@ufl.edu>
2015-02-17 19:34:35 -0500

    Add joinCourse with modal and takes courseCode

--- public/modules/courses/controllers/courses.client.controller.js
+++ public/modules/courses/controllers/courses.client.controller.js
@@ -35,6 +35,35 @@ angular.module('courses').controller('CoursesController', ['$scope', '$statePara
 				$log.info('Modal dismissed at: ' + new Date());
 			});
 		};
+
+		$scope.openJoinCourse = function (size) {
+
+			var modalInstance = $modal.open({
+				templateUrl: 'modules/courses/views/join-course.client.view.html',
+				controller: function ($scope, $modalInstance){
+
+					$scope.ok = function () {
+						//can only close if the form is valid!
+						//eg if the "required" field is filled
+
+						//if (createCustomerForm.$valid){
+							$modalInstance.close($scope.insertedCCode);
+						//}
+					};
+
+					$scope.cancel = function () {
+						$modalInstance.dismiss('cancel');
+					};
+				},
+				size: size
+			});
+
+			modalInstance.result.then(function (selectedItem) {
+				$scope.selected = selectedItem;
+			}, function () {
+				$log.info('Modal dismissed at: ' + new Date());
+			});
+		};
 	
 
 		// Create new Course
@@ -103,6 +132,30 @@ angular.module('courses').controller('CoursesController', ['$scope', '$statePara
 			$scope.course = Courses.get({
 				courseId: $stateParams.courseId
 			});
+		};
+
+		// Join a Course
+		$scope.joinCourse = function() {
+			/* Add save function
+			*  Add checks (see notes)
+			*  View course name?
+			*/ 
+			var course = $scope.course;
+			var user = $scope.authentication.user; 
+			var flag = false;
+			if ($scope.insertedCCode === course.courseCode) {
+				for (var i in user.joinedCourses) {
+					if (user.joinedCourses[i] === course._id) {
+						flag = true;  // Already joined the course
+					}
+				}
+				if (!flag) {
+					user.joinedCourses.push(course._id);
+				}
+			}
+
+
+
 		};
 
 	}
